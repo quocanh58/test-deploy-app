03e321bfa4965551cdf6d4cb53e51723ad2d72eb
ttqanh <anhttq@vnpt.vn>
2025-10-01 16:13:05 +0700

    pushlish swagger to development environment

--- PE_PRN231_SE150627_API/Program.cs
+++ PE_PRN231_SE150627_API/Program.cs
@@ -59,11 +59,14 @@ builder.Services.AddScoped(typeof(GenericRepository<>));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
